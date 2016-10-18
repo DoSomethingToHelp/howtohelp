a26eebb20543dadfe74abc99980b005d3ff6c07d
Brian Pearce <brian.o.pearce@gmail.com>
2016-10-18 21:03:21 +0000

    Break the splash template into partials
    Use partials for sections. Render the Page#Index function with the splash layout.

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule Howtohelp.PageController do
   use Howtohelp.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html",
+      layout: {Howtohelp.LayoutView, "splash.html"}
   end
 end
